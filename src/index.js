ab243c28f3e856c6951867eedaa80c83d4e208ad
Moises <mhdezcoronado@gmail.com>
2021-08-30 21:23:05 +0200

    Add provider & store on index.js

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './calculator.css';
+import { Provider } from 'react-redux';
 import App from './App';
+import store from './redux/cofigureStore';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router basename={process.env.PUBLIC_URL}>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
