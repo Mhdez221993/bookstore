dfb99248b1c65a992ec3fa6438145c429f032f74
Moises <mhdezcoronado@gmail.com>
2021-08-31 23:33:11 +0200

    Add getBook & postBook to redux

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,7 +1,9 @@
+import FetchApi from '../API/api';
+
 const ADD_BOOK = 'bookstore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookstore/books/REMOVE_BOOK';
-
 const initialState = [];
+const fetchApi = new FetchApi();
 
 export const addBook = payload => ({
   type: ADD_BOOK,
@@ -12,6 +14,27 @@ export const removeBook = id => ({
   type: REMOVE_BOOK,
   id,
 });
+
+export const getBooks = () => async dispatch => {
+  const apiBooks = await fetchApi.get();
+  Object.entries(apiBooks).forEach(([id, [{ category, title }]]) => {
+    dispatch(addBook({ category, title, id }));
+  });
+};
+
+export const postBooks = (payload, method) => async dispatch => {
+  if (method === 'POST') {
+    dispatch(addBook(payload));
+  } else {
+    dispatch(removeBook(payload));
+  }
+  await fetchApi.post(payload, method);
+};
+
+// export const deleteBooks = id => async dispatch => {
+//   dispatch(removeBook(id));
+//   await fetchApi.deleteBook(id);
+// };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
