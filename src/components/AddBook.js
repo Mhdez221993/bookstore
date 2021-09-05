a2d0917056d552d8bdce5e23c50e6d17a7dfcbda
Moises <mhdezcoronado@gmail.com>
2021-09-05 15:15:34 +0200

    fix addBook component

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -1,27 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { v4 as uuidv4 } from 'uuid';
-import { postBooks } from '../redux/books/books';
+import { createBook } from '../redux/books/books';
 
 function AddBooks() {
   const dispatch = useDispatch();
+  const [title, setTile] = useState('');
+  const [category, setCategory] = useState('');
 
-  const submitBookToStore = e => {
-    e.preventDefault();
+  const handleTitle = e => setTile(e.target.value);
+  const handleCategory = e => setCategory(e.target.value);
+
+  const handleSubmit = e => {
     const newBook = {
       item_id: uuidv4(),
-      title: e.target.title.value,
-      category: e.target.category.value,
+      title,
+      category,
     };
 
-    dispatch(postBooks(newBook, 'POST'));
-    e.target.title.value = '';
-    e.target.category.value = '';
+    dispatch(createBook(newBook));
+    setTile('');
+    setCategory('');
+    e.preventDefault();
   };
 
   return (
     <div className="submit-book">
-      <form onSubmit={submitBookToStore}>
+      <form onSubmit={handleSubmit}>
         <h3 className="input-book-label">ADD NEW BOOK</h3>
         <div className="wrapper-input">
           <input
@@ -29,12 +34,17 @@ function AddBooks() {
             placeholder="Book title"
             name="title"
             className="book-title-input"
+            value={title}
+            required
+            onChange={handleTitle}
           />
           <input
             type="text"
             placeholder="Category"
             name="category"
             className="book-category-input"
+            value={category}
+            onChange={handleCategory}
           />
           <button className="book-submit" type="submit">ADD BOOK</button>
         </div>
