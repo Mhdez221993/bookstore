de74e4606642488a3532f2576a0ec739f88e1e4b
Moises <mhdezcoronado@gmail.com>
2021-09-05 15:16:12 +0200

    Fix Display book component to not rerender

--- src/components/DisplayBooks.js
+++ src/components/DisplayBooks.js
@@ -1,15 +1,16 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { postBooks, getBooks } from '../redux/books/books';
+import { deleteBook, loadBooks } from '../redux/books/books';
 import ProgresCircle from './ProgresCircle';
 
 function DisplayBooks() {
   const dispatch = useDispatch();
   const books = useSelector(book => book.booksReducer);
+  const handleDeleteBook = id => dispatch(deleteBook(id));
 
   useEffect(
     () => {
-      dispatch(getBooks());
+      dispatch(loadBooks());
     },
     [],
   );
@@ -18,14 +19,14 @@ function DisplayBooks() {
     <div className="DisplayBooks">
       <ul>
         {books.map(book => (
-          <li key={book.id} className="book-list-item">
+          <li key={book.item_id} className="book-list-item">
             <div className="book-info">
               <span className="book-category">{ book.category }</span>
               <span className="book-title">{ book.title }</span>
               <span className="book-author">Book author</span>
               <div className="crud-operations">
                 <button className="crud" type="button">Comments</button>
-                <button className="crud" type="button" onClick={() => dispatch(postBooks(book.id, 'DELETE'))}>Remove</button>
+                <button className="crud" type="button" onClick={() => handleDeleteBook(book.item_id)}>Remove</button>
                 <button className="crud" type="button">Edit</button>
               </div>
             </div>
