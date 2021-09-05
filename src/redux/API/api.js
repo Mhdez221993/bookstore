e4c3bfa876308f647be70b62aa85936d7fe1ecc8
Moises <mhdezcoronado@gmail.com>
2021-09-05 15:17:45 +0200

    Fix API fetch

--- src/redux/API/api.js
+++ src/redux/API/api.js
@@ -1,29 +1,50 @@
-export default class FetchApi {
-  constructor() {
-    this.api = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/1N2yeTElQOxuxd4PIUim/books';
-  }
+const BASE_URL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi';
+const STORAGE_KEY = 'appId';
 
-  async get() {
-    const response = await fetch(this.api)
-      .then(response => response.json());
-    return response;
-  }
+const post = (endpoint, body = {}, method = 'POST') => fetch(`${BASE_URL}${endpoint}`, {
+  method,
+  body: JSON.stringify(body),
+  headers: {
+    'Content-type': 'application/json; charset=UTF-8',
+  },
+});
 
-  async post(payload, method) {
-    const configAdd = {
-      method: 'POST',
-      headers: {
-        'Content-type': 'application/json',
-      },
-      body: JSON.stringify(payload),
-    };
-    const configDelete = { method: 'DELETE' };
-    const config = method === 'POST' ? configAdd : configDelete;
-    const url = method === 'POST' ? this.api : `${this.api}/${payload}`;
+export const createApp = async () => {
+  let appId = localStorage.getItem(STORAGE_KEY);
 
-    await fetch(
-      url,
-      config,
-    );
+  if (!appId) {
+    const response = await post('/apps');
+
+    appId = await response.text();
+    localStorage.setItem(STORAGE_KEY, appId);
   }
-}
+};
+
+export const createBook = async book => {
+  const appId = localStorage.getItem(STORAGE_KEY);
+  const response = await post(`/apps/${appId}/books`, book);
+
+  return response.status === 201;
+};
+
+export const getBooks = async () => {
+  const appId = localStorage.getItem(STORAGE_KEY);
+  const response = await fetch(`${BASE_URL}/apps/${appId}/books`);
+  try {
+    const books = await response.json();
+    return books;
+  } catch (error) {
+    return {};
+  }
+};
+
+export const deleteBook = async id => {
+  const appId = localStorage.getItem(STORAGE_KEY);
+  const response = await post(`/apps/${appId}/books/${id}`, {
+    item_id: id,
+  }, 'DELETE');
+
+  const result = await response.text();
+
+  return result === 'The book was deleted successfully!';
+};
