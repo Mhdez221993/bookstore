74a0e712b9c34cea26b6bac7e11eb3ae86e2b30f
Moises <mhdezcoronado@gmail.com>
2021-11-01 15:17:18 +0100

    Setup Router

--- src/App.js
+++ src/App.js
@@ -1,8 +1,36 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Books from './components/books/Books';
+import Categories from './components/categories/Categories';
+import Nav from './components/Nav';
+
+const routes = [
+  {
+    name: 'BOOKS',
+    path: '/',
+    component: <Books />,
+  },
+  {
+    name: 'CATEGORIES',
+    path: '/categories',
+    component: <Categories />,
+  },
+];
 
 function App() {
   return (
-    <div>Book List App</div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Nav title="BookStore CMS" routes={routes} />
+      <Switch>
+        {
+          routes.map(
+            ({ path, component }) => (
+              <Route exact key={path} path={path}>{ component }</Route>
+            ),
+          )
+        }
+      </Switch>
+    </Router>
   );
 }
 
