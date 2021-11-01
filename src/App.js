265a9582f7b2fd00b519a4a68578f93c93c9ddea
Moises <mhdezcoronado@gmail.com>
2021-11-01 12:45:57 +0100

    Add first setup

--- src/App.js
+++ src/App.js
@@ -1,27 +1,8 @@
 import React from 'react';
-import {
-  Switch,
-  Route,
-} from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Books from './components/Books';
-import Categories from './components/Categories';
 
 function App() {
   return (
-    <div className="App">
-      <Navbar />
-      <div className="Main">
-        <Switch>
-          <Route exact path="/">
-            <Books />
-          </Route>
-          <Route exact path="/categories">
-            <Categories />
-          </Route>
-        </Switch>
-      </div>
-    </div>
+    <div>Book List App</div>
   );
 }
 
