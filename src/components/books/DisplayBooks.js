89692feef1efeaf6382772458a08c2b51f3ee941
Moises <mhdezcoronado@gmail.com>
2021-11-02 12:17:17 +0100

    Add onClick to dispatch removeBook action

--- src/components/books/DisplayBooks.js
+++ src/components/books/DisplayBooks.js
@@ -1,35 +1,49 @@
 import PropTypes from 'prop-types';
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { removeBook } from '../../redux/books/books';
 import ProgresCircle from './ProgresCircle';
 
-const DisplayBooks = ({ title, author, category }) => (
+const DisplayBooks = ({
+  id, title, author, category,
+}) => {
+  const dispatch = useDispatch();
 
-  <>
-    <div className="book-info">
-      <span className="book-category">{ category }</span>
-      <br />
-      <span className="book-title">{ title }</span>
-      <br />
-      <span className="book-category">{ author }</span>
-      <br />
-      <div className="crud-operations">
-        <button className="crud" type="button">Comments</button>
-        <button className="crud" type="button">Remove</button>
-        <button className="crud" type="button">Edit</button>
+  return (
+    <>
+      <div className="book-info">
+        <span className="book-category">{ category }</span>
+        <br />
+        <span className="book-title">{ title }</span>
+        <br />
+        <span className="book-category">{ author }</span>
+        <br />
+        <div className="crud-operations">
+          <button className="crud" type="button">Comments</button>
+          <button
+            className="crud"
+            type="button"
+            onClick={() => dispatch(removeBook(id))}
+          >
+            Remove
+          </button>
+          <button className="crud" type="button">Edit</button>
+        </div>
       </div>
-    </div>
-    <div className="book-progress">
-      <ProgresCircle />
-    </div>
-    <div className="book-chapteer">
-      <span className="chapter-items first">Current Chapter</span>
-      <span className="chapter-items">Chapter 17</span>
-      <div className="chapter-items"><button className="update-progress" type="button">Update progres</button></div>
-    </div>
-  </>
-);
+      <div className="book-progress">
+        <ProgresCircle />
+      </div>
+      <div className="book-chapteer">
+        <span className="chapter-items first">Current Chapter</span>
+        <span className="chapter-items">Chapter 17</span>
+        <div className="chapter-items"><button className="update-progress" type="button">Update progres</button></div>
+      </div>
+    </>
+  );
+};
 
 DisplayBooks.propTypes = {
+  id: PropTypes.string.isRequired,
   author: PropTypes.string.isRequired,
   category: PropTypes.string.isRequired,
   title: PropTypes.string.isRequired,
