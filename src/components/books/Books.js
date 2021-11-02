ac8a0d939f9df3524ca32113caefcd5e349ed36e
Moises <mhdezcoronado@gmail.com>
2021-11-02 11:57:44 +0100

    Remove initial state and update props

--- src/components/books/Books.js
+++ src/components/books/Books.js
@@ -1,32 +1,25 @@
 import React from 'react';
-import { v4 as uuidv4 } from 'uuid';
+import { useSelector } from 'react-redux';
 import AddBooks from './AddBook';
 import DisplayBooks from './DisplayBooks';
 
-const books = [
-  {
-    id: uuidv4(),
-    title: 'Book title 1',
-    category: 'sfy',
-  },
-  {
-    id: uuidv4(),
-    title: 'Book title 2',
-    category: 'sfy',
-  },
-];
-
-const Books = () => (
-  <div className="Books">
-    <ul>
-      { books.map(({ id, title, category }) => (
-        <li key={id}>
-          <DisplayBooks title={title} category={category} />
-        </li>
-      ))}
-    </ul>
-    <AddBooks />
-  </div>
-);
+const Books = () => {
+  const books = useSelector(books => books.booksReducer);
+  return (
+    <div className="Books">
+      <ul>
+        { books.map(({
+          id, title, author, category,
+        }) => (
+          <li key={id}>
+            <DisplayBooks title={title} author={author} category={category} />
+            <br />
+          </li>
+        ))}
+      </ul>
+      <AddBooks />
+    </div>
+  );
+};
 
 export default Books;
