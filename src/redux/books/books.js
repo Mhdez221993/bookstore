371c76699edaa287a7b1a93e957c7d3b67182af6
Moises <mhdezcoronado@gmail.com>
2021-11-02 11:59:29 +0100

    Create initialState

--- src/redux/books/books.js
+++ src/redux/books/books.js
@@ -1,28 +1,61 @@
+import { v4 as uuidv4 } from 'uuid';
+
 const ADD_BOOK = 'bookStore/books/ADD_BOOK';
 const REMOVE_BOOK = 'bookStore/books/REMOVE_BOOK';
 
-const initialState = [];
+const initialState = [
+  {
+    id: uuidv4(),
+    category: 'Action',
+    title: 'The Hunger Games',
+    author: 'Suzanne Collins',
+    progress: {
+      currentChapter: 'Chapter 17',
+      completed: '64',
+    },
+  },
+  {
+    id: uuidv4(),
+    category: 'Science Fiction',
+    title: 'Dune',
+    author: 'Frank Herbert',
+    progress: {
+      currentChapter: 'Chapter 3: "A Lesson Learned"',
+      completed: '8',
+    },
+  },
+  {
+    id: uuidv4(),
+    category: 'Economy',
+    title: 'Capital in the Twenty-First Century',
+    author: 'Suzanne Collins',
+    progress: {
+      currentChapter: 'Introduction',
+      completed: '0',
+    },
+  },
+];
 
 export const addBook = payload => ({
-    type: ADD_BOOK,
-    payload
-})
+  type: ADD_BOOK,
+  payload,
+});
 export const removeBook = payload => ({
   type: REMOVE_BOOK,
-  payload
-})
+  payload,
+});
 
-const reducer =  (state = initialState, action) => {
-switch (action.type) {
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
     case ADD_BOOK:
-        return [ ...state, action.payload];
+      return [...state, action.payload];
 
     case REMOVE_BOOK:
-        return state.filter(book => book.id !== id);
+      return state.filter(book => book.id !== action.payload);
 
     default:
-        return state;
-    }
+      return state;
+  }
 };
 
 export default reducer;
