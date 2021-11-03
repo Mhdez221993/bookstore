7728d753cc3ce9697dfa789b85b22bc61ec6637b
Moises <mhdezcoronado@gmail.com>
2021-11-03 13:16:51 +0100

    import usseEffect & distpatch loadBooks

--- src/components/books/Books.js
+++ src/components/books/Books.js
@@ -1,18 +1,31 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { loadBooks } from '../../redux/books/books';
 import AddBooks from './AddBook';
 import DisplayBooks from './DisplayBooks';
 
 const Books = () => {
+  const dispatch = useDispatch();
   const books = useSelector(books => books.booksReducer);
+
+  useEffect(
+    () => {
+      dispatch(loadBooks());
+    },
+    [],
+  );
+
   return (
     <div className="Books">
       <ul>
-        { books.map(({
-          id, title, author, category,
-        }) => (
-          <li key={id}>
-            <DisplayBooks id={id} title={title} author={author} category={category} />
+        { books.map(book => (
+          <li key={book.item_id}>
+            <DisplayBooks
+              id={book.item_id}
+              title={book.title}
+              author={book.author}
+              category={book.category}
+            />
             <br />
           </li>
         ))}
