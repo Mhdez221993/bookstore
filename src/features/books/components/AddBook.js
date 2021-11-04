805a41728899797c024a5e8f188c42543d5c5564
Moises <mhdezcoronado@gmail.com>
2021-11-04 10:23:13 +0100

    Update path file to match Ducks patter

---
+++ src/features/books/components/AddBook.js
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { v4 as uuidv4 } from 'uuid';
+import { createBook } from '../booksReducer';
+import './addBooks.css';
+
+const AddBooks = () => {
+  const dispatch = useDispatch();
+  const [title, setTitle] = useState('');
+  const [category, setcategory] = useState('');
+
+  const handleTitle = e => setTitle(e.target.value);
+  const handleAuthor = e => setcategory(e.target.value);
+
+  const submitBookToStore = e => {
+    const newBook = {
+      item_id: uuidv4(),
+      title,
+      category,
+    };
+
+    dispatch(createBook(newBook));
+    setTitle('');
+    setcategory('');
+    e.preventDefault();
+  };
+
+  return (
+    <div className="submit-book">
+      <form onSubmit={submitBookToStore}>
+        <h3 className="input-book-label">ADD NEW BOOK</h3>
+        <div className="wrapper-input">
+          <input
+            type="text"
+            placeholder="Book title"
+            name="title"
+            className="book-title-input"
+            value={title}
+            required
+            onChange={handleTitle}
+          />
+          <input
+            type="text"
+            placeholder="Category"
+            name="author"
+            className="book-author-input"
+            value={category}
+            onChange={handleAuthor}
+          />
+          <button
+            className="book-submit"
+            type="submit"
+          >
+            ADD BOOK
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default AddBooks;
