dd6ca366788fde86504f54b8962e7c05d064d0f8
Moises <mhdezcoronado@gmail.com>
2021-11-04 10:22:29 +0100

    Update path file to match Ducks patter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import App from './App';
-import store from './redux/cofigureStore';
+import App from './app/App';
+import store from './app/store';
 
 ReactDOM.render(
   <React.StrictMode>
